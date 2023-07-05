9b50876b91e3a5c61f1c0ebfbf67c2608a7d74c5
owen-eternal <ptarworx@gmail.com>
2023-07-05 22:01:27 +0200

    Remove sensitive information

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,13 +5,5 @@ terraform {
       version = "5.5.0"
     }
   }
-  backend "s3" {
-    bucket = "tf-s3-remote-state-bucket"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
-
-provider "aws" {
-  region  = "eu-west-1"
+  backend "s3" {}
 }
