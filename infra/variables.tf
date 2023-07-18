0f6aaa0aebede460a9b48620e772fa817525df7b
owen-eternal <ptarworx@gmail.com>
2023-07-18 22:56:25 +0200

    define network configuration

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,16 +1,57 @@
 variable "environment" {
-  type = map(string)
+  type = map(any)
   default = {
-    "production" = true,
-    "staging"    = true
+    "production" = {
+      "apply"      = true
+      "network_id" = "10.0.0.0/16"
+      "subnets" = {
+        "web_aza" = {
+          "network_id"              = "10.0.0.0/18"
+          "map_public_ip_on_launch" = true
+          "availability_zone"       = "eu-west-1a"
+        },
+        "web_azb" = {
+          "network_id"              = "10.0.64.0/18"
+          "map_public_ip_on_launch" = true
+          "availability_zone"       = "eu-west-1b"
+        },
+        "data_aza" = {
+          "network_id"        = "10.0.128.0/18"
+          "map_public_ip_on_launch" = false
+          "availability_zone" = "eu-west-1a"
+        },
+        "data_azb" = {
+          "network_id"        = "10.0.192.0/18"
+          "map_public_ip_on_launch" = false
+          "availability_zone" = "eu-west-1b"
+        }
+      }
+    },
+    "staging" = {
+      "apply"      = true
+      "network_id" = "192.168.0.0/16"
+      "subnets" = {
+        "web_aza" = {
+          "network_id"              = "192.168.0.0/18"
+          "map_public_ip_on_launch" = true
+          "availability_zone"       = "eu-west-1a"
+        },
+        "web_azb" = {
+          "network_id"              = "192.168.64.0/18"
+          "map_public_ip_on_launch" = true
+          "availability_zone"       = "eu-west-1b"
+        },
+        "data_aza" = {
+          "network_id"        = "192.168.128.0/18"
+          "map_public_ip_on_launch" = false
+          "availability_zone" = "eu-west-1a"
+        },
+        "data_azb" = {
+          "network_id"        = "192.168.192.0/18"
+          "map_public_ip_on_launch" = false
+          "availability_zone" = "eu-west-1b"
+        }
+      }
+    }
   }
 }
-
-variable "env_cblocks" {
-  type = map(string)
-  default = {
-    "production" = "10.0.0.0/16"
-    "staging"    = "192.168.0.0/16"
-  }
-}
-
