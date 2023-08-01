f570d31f8139f9f862248ca084da57b889c6512e
owen-eternal <ptarworx@gmail.com>
2023-08-01 22:45:08 +0200

    Define vars needed by the module

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,57 +1,9 @@
-variable "environment" {
-  type = map(any)
-  default = {
-    "production" = {
-      "apply"      = true
-      "network_id" = "10.0.0.0/16"
-      "subnets" = {
-        "web_aza" = {
-          "network_id"              = "10.0.0.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1a"
-        },
-        "web_azb" = {
-          "network_id"              = "10.0.64.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1b"
-        },
-        "data_aza" = {
-          "network_id"              = "10.0.128.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1a"
-        },
-        "data_azb" = {
-          "network_id"              = "10.0.192.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1b"
-        }
-      }
-    },
-    "staging" = {
-      "apply"      = true
-      "network_id" = "192.168.0.0/16"
-      "subnets" = {
-        "web_aza" = {
-          "network_id"              = "192.168.0.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1a"
-        },
-        "web_azb" = {
-          "network_id"              = "192.168.64.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1b"
-        },
-        "data_aza" = {
-          "network_id"              = "192.168.128.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1a"
-        },
-        "data_azb" = {
-          "network_id"              = "192.168.192.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1b"
-        }
-      }
-    }
-  }
-}
+####### VPC-Tier Module Variables ########
+variable "vpc_cdir" {}
+variable "project_name" {}
+variable "environment" {}
+variable "subnet_cdir" {}
+variable "web_server_port" {}
+variable "database" {}
+variable "db_server_port" {}
+variable "ipaddr" {}
