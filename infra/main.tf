cd6b1330f4b74f583dfd5544ed6f6c36e6d841e8
owen-eternal <ptarworx@gmail.com>
2023-08-01 22:44:41 +0200

    Import vpc module

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,11 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = var.environment[terraform.workspace].network_id
-
-  tags = {
-    Name = "${terraform.workspace}-network"
-  }
-}
-
-resource "aws_subnet" "subnet" {
-  for_each                = var.environment[terraform.workspace].subnets
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = each.value.network_id
-  availability_zone       = each.value.availability_zone
-  map_public_ip_on_launch = each.value.map_public_ip_on_launch
-
-  tags = {
-    Name = "${terraform.workspace}-${each.key}"
-  }
+module "tf-aws-network" {
+  source          = "git@github.com:owen-eternal/tf-aws-network-mod.git"
+  vpc_cdir        = var.vpc_cdir
+  project_name    = var.project_name
+  environment     = terraform.workspace
+  web_server_port = var.web_server_port
+  database        = var.database
+  db_server_port  = var.db_server_port
+  subnet_cdir     = var.subnet_cdir
+  ipaddr          = var.ipaddr
 }
