b77df70200b6018d94f71b153968784b5f6fa4fe
owen-eternal <ptarworx@gmail.com>
2023-08-03 12:31:11 +0200

    fix styling

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,22 +4,22 @@ variable "database" {}
 variable "ipaddr" {}
 
 variable "network" {
-    description = "Object with a list of CIDR block ranges for the network."
-    type = map(any)
-    default = {
-        "staging" = {
-            "vid" = "10.0.0.0/16"
-            "subnets" = {
-                "web" = ["10.0.0.0/18", "10.0.64.0/18"]
-                "db"  = []
-            }
-        }
-        "production" = {
-            "vid" = "172.0.0.0/16"
-            "subnets" = {
-                "web" = ["172.0.0.0/18", "172.0.64.0/18"]
-                "db"  = []
-            }
-        }
+  description = "Object with a list of CIDR block ranges for the network."
+  type        = map(any)
+  default = {
+    "staging" = {
+      "vid" = "10.0.0.0/16"
+      "subnets" = {
+        "web" = ["10.0.0.0/18", "10.0.64.0/18"]
+        "db"  = []
+      }
     }
+    "production" = {
+      "vid" = "172.0.0.0/16"
+      "subnets" = {
+        "web" = ["172.0.0.0/18", "172.0.64.0/18"]
+        "db"  = []
+      }
+    }
+  }
 }
