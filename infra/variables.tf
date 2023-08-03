8d11f43597007010ca5bfc25dd5fe695e6bc9d97
owen-eternal <ptarworx@gmail.com>
2023-08-03 11:55:16 +0200

    fix map obj

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,18 +6,18 @@ variable "workspace" {}
 variable "network" {
     type = map(any)
     default = {
-        staging = {
-            vid = "10.0.0.0/16"
-            subnets = {
-                web = ["10.0.0.0/18", "10.0.64.0/18"]
-                db  = []
+        "staging" = {
+            "vid" = "10.0.0.0/16"
+            "subnets" = {
+                "web" = ["10.0.0.0/18", "10.0.64.0/18"]
+                "db"  = []
             }
         }
-        production = {
-            network = "172.0.0.0/16"
-            subnets = {
-                web = ["172.0.0.0/18", "172.0.64.0/18"]
-                db  = []
+        "production" = {
+            "vid" = "172.0.0.0/16"
+            "subnets" = {
+                "web" = ["172.0.0.0/18", "172.0.64.0/18"]
+                "db"  = []
             }
         }
     }
