c84099b60880a920c93312d2087ce5cd7d09e74d
Owen Phakade <68030544+owen-eternal@users.noreply.github.com>
2023-08-03 12:40:23 +0200

    Feature/import vpc network module (#7)
    * Import the tf-aws-network-module.
    * Create a network object with a list of subnet and vpc CIDR block ranges.
    * Remove unwanted variables.

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,11 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = var.environment[terraform.workspace].network_id
-
-  tags = {
-    Name = "${terraform.workspace}-network"
-  }
-}
-
-resource "aws_subnet" "subnet" {
-  for_each                = var.environment[terraform.workspace].subnets
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = each.value.network_id
-  availability_zone       = each.value.availability_zone
-  map_public_ip_on_launch = each.value.map_public_ip_on_launch
-
-  tags = {
-    Name = "${terraform.workspace}-${each.key}"
-  }
+module "tf-aws-network" {
+  source          = "git@github.com:owen-eternal/tf-aws-network-mod.git"
+  db_server_port  = null
+  database        = var.database
+  environment     = terraform.workspace
+  ipaddr          = var.ipaddr
+  project_name    = "hyroku"
+  subnet_cdir     = var.network[terraform.workspace].subnets
+  vpc_cdir        = var.network[terraform.workspace].vid
+  web_server_port = null
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,56 +1,24 @@
-variable "environment" {
-  type = map(any)
+####### VPC-Tier Module Variables ########
+variable "database" {}
+
+variable "ipaddr" {}
+
+variable "network" {
+  description = "Object with a list of CIDR block ranges for the network."
+  type        = map(any)
   default = {
-    "production" = {
-      "apply"      = true
-      "network_id" = "10.0.0.0/16"
-      "subnets" = {
-        "web_aza" = {
-          "network_id"              = "10.0.0.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1a"
-        },
-        "web_azb" = {
-          "network_id"              = "10.0.64.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1b"
-        },
-        "data_aza" = {
-          "network_id"              = "10.0.128.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1a"
-        },
-        "data_azb" = {
-          "network_id"              = "10.0.192.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1b"
-        }
-      }
-    },
     "staging" = {
-      "apply"      = true
-      "network_id" = "192.168.0.0/16"
+      "vid" = "10.0.0.0/16"
       "subnets" = {
-        "web_aza" = {
-          "network_id"              = "192.168.0.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1a"
-        },
-        "web_azb" = {
-          "network_id"              = "192.168.64.0/18"
-          "map_public_ip_on_launch" = true
-          "availability_zone"       = "eu-west-1b"
-        },
-        "data_aza" = {
-          "network_id"              = "192.168.128.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1a"
-        },
-        "data_azb" = {
-          "network_id"              = "192.168.192.0/18"
-          "map_public_ip_on_launch" = false
-          "availability_zone"       = "eu-west-1b"
-        }
+        "web" = ["10.0.0.0/18", "10.0.64.0/18"]
+        "db"  = []
+      }
+    }
+    "production" = {
+      "vid" = "172.0.0.0/16"
+      "subnets" = {
+        "web" = ["172.0.0.0/18", "172.0.64.0/18"]
+        "db"  = []
       }
     }
   }
