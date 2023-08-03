fda24e5603d4729a0b73d9b16b04f9c6291e8f57
owen-eternal <ptarworx@gmail.com>
2023-08-03 10:50:25 +0200

    fix map

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,12 +5,12 @@ variable "network" {
     type = map(any)
     default = {
         "staging" = {
-           "vid" = "10.0.0.0/16"
+            "vid" = "10.0.0.0/16"
             "subnets" = {
                 "web" = ["10.0.0.0/18", "10.0.64.0/18"]
                 "db"  = []
             }
-        },
+        }
         "production" = {
             "network" = "172.0.0.0/16"
             "subnets" = {
