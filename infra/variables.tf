eee9d78703d82d2496d2161cf56a622f01bbf1cd
owen-eternal <ptarworx@gmail.com>
2023-08-03 11:51:23 +0200

    debug

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,22 +6,18 @@ variable "workspace" {}
 variable "network" {
     type = map(any)
     default = {
-        "default" = {
-            "vid" = null
-            "subnets" = null
-        } 
-        "staging" = {
-            "vid" = "10.0.0.0/16"
-            "subnets" = {
-                "web" = ["10.0.0.0/18", "10.0.64.0/18"]
-                "db"  = []
+        staging = {
+            vid = "10.0.0.0/16"
+            subnets = {
+                web = ["10.0.0.0/18", "10.0.64.0/18"]
+                db  = []
             }
         }
-        "production" = {
-            "network" = "172.0.0.0/16"
-            "subnets" = {
-                "web" = ["172.0.0.0/18", "172.0.64.0/18"]
-                "db"  = []
+        production = {
+            network = "172.0.0.0/16"
+            subnets = {
+                web = ["172.0.0.0/18", "172.0.64.0/18"]
+                db  = []
             }
         }
     }
